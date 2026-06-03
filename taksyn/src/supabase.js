89091a69c8a8eaa0ba46640426b751c754c06a6c
olafdierich <olafdierich123@yahoo.com.au>
2026-06-03 14:52:46 +1000

    Update supabase.js

--- taksyn/src/supabase.js
+++ taksyn/src/supabase.js
@@ -10,7 +10,7 @@ export const supabase = createClient(
     auth: {
       persistSession: true,
       autoRefreshToken: true,
-      detectSessionInUrl: false,
+      detectSessionInUrl: true,
       storageKey: 'taksyn-auth',
     },
     global: {
